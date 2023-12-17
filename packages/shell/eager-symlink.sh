51bdc6c44ed808ad0f13d242d6fcd185a2b4d333
Maxence <m.jauan@gmail.com>
2023-12-17 09:10:48 +0100

    🌳 MFE: Fix images.

--- packages/shell/eager-symlink.sh
+++ packages/shell/eager-symlink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(unlink ./public/mfe-1-v1 || rm -Rf ./public/mfe-1-v1) && cp -R ../remote-mfe-1/dist ./public/mfe-1-v1
-(unlink ./public/layout-v1 || rm -Rf ./public/layout-v1) && cp -R ../layout/dist ./public/layout-v1
+(unlink ./public/remote-mfe-1-0.0.0 || rm -Rf ./public/remote-mfe-1-0.0.0) && cp -R ../remote-remote-mfe-1/dist ./public/remote-mfe-1-0.0.0
+(unlink ./public/layout-0.0.0 || rm -Rf ./public/layout-0.0.0) && cp -R ../layout/dist ./public/layout-0.0.0
