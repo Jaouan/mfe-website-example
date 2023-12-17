815c8daafb674ca3b9abb315b7511599794d1ff5
Maxence <m.jauan@gmail.com>
2023-12-17 09:22:16 +0100

    🐛 Fix eagerify modules.

--- packages/shell/eager-symlink.sh
+++ packages/shell/eager-symlink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(unlink ./public/remote-mfe-1-0.0.0 || rm -Rf ./public/remote-mfe-1-0.0.0) && cp -R ../remote-remote-mfe-1/dist ./public/remote-mfe-1-0.0.0
+(unlink ./public/remote-mfe-1-0.0.0 || rm -Rf ./public/remote-mfe-1-0.0.0) && cp -R ../remote-mfe-1/dist ./public/remote-mfe-1-0.0.0
 (unlink ./public/layout-0.0.0 || rm -Rf ./public/layout-0.0.0) && cp -R ../layout/dist ./public/layout-0.0.0
