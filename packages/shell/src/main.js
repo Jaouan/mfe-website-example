566d8dde259aa9c2a8eaf9dd4ef1bf32557ebbe4
Maxence <m.jauan@gmail.com>
2023-12-18 22:24:10 +0100

    🧐 Vercel: add speed insights.

--- packages/shell/src/main.js
+++ packages/shell/src/main.js
@@ -1,1 +1,4 @@
+import { injectSpeedInsights } from "@vercel/speed-insights";
+import.meta.env.PROD && injectSpeedInsights({ route: document.location.pathname || "/" });
+
 (await import("layout/App")).default.mount(document.getElementById("layout"));
