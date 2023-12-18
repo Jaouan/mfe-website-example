344f70e1dbcd3b72a100c4ed13aa1c0ad5a1885f
zndi <85784039+zndi@users.noreply.github.com>
2023-12-18 23:44:34 +0100

    🐛 Fix Safari multi-promises issue

--- packages/layout/src/navigation/module-loader.js
+++ packages/layout/src/navigation/module-loader.js
@@ -13,17 +13,8 @@ const addRemote = (scope, url, opts = {}) =>
         ...opts
     });
 
-const getRemote = async (scope, module = "./App") => {
-    // On iOS & Safari, getting the same module multiple times simultaneously may lead to unexpected errors.
-    // Hack: Retry 2400 times * 25ms (1 minute).
-    for (let retry = 0; retry < 2400; retry++) {
-        try {
-            return await __federation_method_getRemote(scope, module);
-        } catch (err) {
-            await new Promise((resolve) => setTimeout(resolve, 25));
-        }
-    }
-}
+const getRemote = (scope, module = "./App") =>
+    __federation_method_getRemote(scope, module);
 
 export const importRemote = (scope, url, module = "./App") => {
     addRemote(scope, url);
