332c681032e67d08cd641d01632f1a5653b86ec2
Maxence <m.jauan@gmail.com>
2023-12-28 23:35:03 +0100

    😌 MFE basic: simplify UI.

--- packages/remote-mfe-basic/src/App.jsx
+++ packages/remote-mfe-basic/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="react-module">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,25 +31,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React module</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           react {count}
         </button>
-        <button onClick={() => increasePopulation()}>
-          global {bears}
-        </button>
+        <button onClick={() => increasePopulation()}>global {bears}</button>
         <button onClick={() => persistedIncreasePopulation()}>
           persisted global {bearsPersisted}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
