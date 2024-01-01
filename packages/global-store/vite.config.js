c3b40a8f194f1e270c2d94462f47327e783970ed
Maxence <m.jauan@gmail.com>
2024-01-01 16:12:58 +0100

    ⚡️  Store: optimize chunk.

--- packages/global-store/vite.config.js
+++ packages/global-store/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
-  plugins: [
-    federation({
-      name: 'mfe-basic',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './global-store.js',
-      }
-    })],
+  build: {
+    lib: {
+      entry: "global-store.js",
+      formats: ["es"]
+    },
+    rollupOptions: {
+      external: ["zustand/vanilla", "zustand/middleware"],
+    }
+  }
 })
